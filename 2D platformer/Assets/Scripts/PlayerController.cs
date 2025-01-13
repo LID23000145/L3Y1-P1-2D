84f19663cfe14945df63f7b873b2589afb8cf886
LID23000145 <LID23000145@heartofyorkshire.ac.uk>
2025-01-13 13:01:56 +0000

    graphsettings

--- 2D platformer/Assets/Scripts/PlayerController.cs
+++ 2D platformer/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
